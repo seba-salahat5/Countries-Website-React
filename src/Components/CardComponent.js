e8ae33d8239bf974bf00eb42e1d1f0d9b092bc4c
seba-salahat5 <80277805+seba-salahat5@users.noreply.github.com>
2023-03-20 15:27:10 +0200

    commit2

--- src/Components/CardComponent.js
+++ src/Components/CardComponent.js
@@ -1,48 +1,44 @@
 import * as React from 'react';
-import { Card, CardActions, CardContent, CardMedia, IconButton, Typography } from '@mui/material';
+import { Card, CardActions,CardActionArea, CardContent, CardMedia, IconButton, Typography } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
 import { THEME } from '../App';
 import StarOutlineRoundedIcon from '@mui/icons-material/StarOutlineRounded';
 import StarRoundedIcon from '@mui/icons-material/StarRounded';
 import { Link } from 'react-router-dom';
 
-let starFillFlag; 
-const linkStyle = {
-    textDecoration: "none",
-    color: '#111517'
-};
+let starFillFlag;
 export default function CountryCardComponent(props) {
     starFillFlag = props.name === "Germany";
     return (
         <Card sx={{ maxWidth: 300 }}>
-            <CardMedia
-                component="img"
-                alt={props.name}
-                height="140"
-                image={props.flag}
-            />
-            <CardContent>
-                <ThemeProvider theme={THEME}>
-                    <Typography gutterBottom variant="h5" component="div">
-                        <Link to="/details" style={linkStyle}>{props.name}</Link>
-                    </Typography>
-                    <Typography gutterBottom variant="h4" component="div">
-                        Population:
-                        <Typography gutterBottom variant="h3" component="span">{props.population}</Typography>
-                    </Typography>
-                    <Typography gutterBottom variant="h4" component="div">
-                        Region:
-                        <Typography gutterBottom variant="h3" component="span">{props.region}</Typography>
-                    </Typography>
-                    <Typography gutterBottom variant="h4" component="div">
-                        Capital:
-                        <Typography gutterBottom variant="h3" component="span">{props.capital}</Typography>
-                    </Typography>
-                </ThemeProvider>
-            </CardContent>
-            <CardActions>
-                {favourateIcon()}
-            </CardActions>
+            <CardActionArea component={Link} to="/details">
+                <CardMedia
+                    component="img"
+                    alt={props.name}
+                    height="140"
+                    image={props.flag}
+                />
+                <CardContent>
+                    <ThemeProvider theme={THEME}>
+                        <Typography gutterBottom variant="h5" component="div">{props.name}</Typography>
+                        <Typography gutterBottom variant="h4" component="div">
+                            Population:
+                            <Typography gutterBottom variant="h3" component="span">{props.population}</Typography>
+                        </Typography>
+                        <Typography gutterBottom variant="h4" component="div">
+                            Region:
+                            <Typography gutterBottom variant="h3" component="span">{props.region}</Typography>
+                        </Typography>
+                        <Typography gutterBottom variant="h4" component="div">
+                            Capital:
+                            <Typography gutterBottom variant="h3" component="span">{props.capital}</Typography>
+                        </Typography>
+                    </ThemeProvider>
+                </CardContent>
+                <CardActions>
+                    {favourateIcon()}
+                </CardActions>
+            </CardActionArea>
         </Card>
     );
 }
